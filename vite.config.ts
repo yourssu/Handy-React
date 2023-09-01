cb4335de3d1c45119fd838593e8387850c7dcd03
혀느현스 <hyuns@hyuns.dev>
2023-09-01 20:35:48 +0900

    feat: 빌드 라이브러리, 설정 파일 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import path from "path";
+import dts from "vite-plugin-dts";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+        }),
+        tsconfigPaths(),
+    ],
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.tsx"),
+            name: "@yourssu/design-system-react",
+            formats: ["es", "cjs"],
+            fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+            external: [
+                "react",
+                "react-dom",
+                "styled-components",
+                "@phosphor-icons/react",
+                "**/*.stories.tsx",
+            ],
+            output: {
+                globals: {
+                    react: "React",
+                    "react-dom": "ReactDOM",
+                    "styled-components": "styled",
+                },
+                banner: '"use client";',
+                interop: "compat",
+            },
+        },
+        ssr: false,
+    },
+});
