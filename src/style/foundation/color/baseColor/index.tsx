22f04018bae8e3f056d6f74c2244305ce59ccea4
혀느현스 <hyuns@hyuns.dev>
2023-09-04 09:43:28 +0900

    feat: CSS Variables를 이용한 구현 삭제

--- src/style/foundation/color/baseColor/index.tsx
+++ src/style/foundation/color/baseColor/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-refresh/only-export-components */
 
-export { baseColorDefinition, baseColorVars } from "./baseColor";
 export { baseColorPalettes } from "./baseColorPalette";
 export type { BaseColorPalette } from "./baseColorPalette";
 export type * from "./baseColor.type";
