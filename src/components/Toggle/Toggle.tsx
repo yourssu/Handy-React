7cd8d0bdc774118291d1703bf3d43c06d219a3a9
nijuy <youjin6325@naver.com>
2023-10-08 01:53:48 +0900

    feat(component): Toggle 컴포넌트 추가
    * <Toggle> 컴포넌트 추가

---
+++ src/components/Toggle/Toggle.tsx
@@ -0,0 +1,40 @@
+import { MouseEvent, forwardRef, useRef } from 'react';
+
+import { StyledToggle, StyeldInput, StyledTrack, StyledThumb } from './Toggle.style';
+import { ToggleProps } from './Toggle.type';
+
+export const Toggle = forwardRef<HTMLDivElement, ToggleProps>(
+  ({ isDisabled = false, isSelected = false, ...props }, ref) => {
+    const toggleRef = useRef<HTMLInputElement | null>(null);
+
+    const handleToggleClick = (event: MouseEvent) => {
+      event.preventDefault();
+
+      if (isDisabled) return;
+      if (toggleRef.current) {
+        toggleRef.current.checked = !toggleRef.current.checked;
+      }
+    };
+
+    return (
+      <StyledToggle
+        ref={ref}
+        onClick={(event) => {
+          handleToggleClick(event);
+        }}
+      >
+        <StyeldInput
+          ref={toggleRef}
+          type="checkbox"
+          disabled={isDisabled}
+          checked={isSelected}
+          {...props}
+          readOnly
+        />
+        <StyledTrack $isDisabled={isDisabled} $isSelected={isSelected}>
+          <StyledThumb $isDisabled={isDisabled} $isSelected={isSelected} />
+        </StyledTrack>
+      </StyledToggle>
+    );
+  }
+);
