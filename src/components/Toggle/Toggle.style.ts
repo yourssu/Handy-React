4fd3ec1c1c32c0ea7ae1f37beab0b50dcc92e9f8
nijuy <youjin6325@naver.com>
2023-10-08 01:51:26 +0900

    feat(component): Toggle.style.ts 추가
    * <Toggle> 컴포넌트 제작을 위한 styled 요소 생성

---
+++ src/components/Toggle/Toggle.style.ts
@@ -0,0 +1,83 @@
+import { styled } from 'styled-components';
+import { DefaultTheme } from 'styled-components/dist/types';
+
+import { ToggleProps } from './Toggle.type';
+
+interface StyledToggleProps {
+  $isDisabled: ToggleProps['isDisabled'];
+  $isSelected: ToggleProps['isSelected'];
+}
+
+const TRACK_WIDTH = 51;
+const TRACK_HEIGHT = 31;
+const THUMB_SIZE = 27;
+const PADDING = 2;
+
+const setTrackColor = ({
+  $isDisabled,
+  $isSelected,
+  theme,
+}: {
+  $isDisabled: StyledToggleProps['$isDisabled'];
+  $isSelected: StyledToggleProps['$isSelected'];
+  theme: DefaultTheme;
+}) => {
+  if ($isDisabled) return theme.color.buttonBG;
+
+  if ($isSelected) return theme.color.buttonPoint;
+  else return theme.color.buttonBG;
+};
+
+export const StyeldInput = styled.input`
+  display: none;
+`;
+
+export const StyledThumb = styled.span<StyledToggleProps>`
+  display: inline-block;
+  width: ${THUMB_SIZE}px;
+  height: ${THUMB_SIZE}px;
+  border: ${({ theme }) => `0.1px solid ${theme.color.borderNormal}`};
+  border-radius: 50%;
+  background-color: ${({ theme, $isDisabled }) =>
+    $isDisabled ? theme.color.buttonDisabled : theme.color.buttonBright};
+  transform: ${({ $isSelected }) =>
+    $isSelected && `translateX(${TRACK_WIDTH - 2 * PADDING - THUMB_SIZE}px)`};
+  transition: 100ms ease-in-out;
+`;
+
+export const StyledTrack = styled.label<StyledToggleProps>`
+  display: inline-block;
+  width: ${TRACK_WIDTH}px;
+  height: ${TRACK_HEIGHT}px;
+  padding: ${PADDING}px;
+  border-radius: 15px;
+  background-color: ${({ theme, $isDisabled, $isSelected }) =>
+    setTrackColor({ $isDisabled, $isSelected, theme })};
+  cursor: ${({ $isDisabled }) => ($isDisabled ? 'not-allowed' : 'pointer')};
+
+  ${StyeldInput}:checked + ${StyledThumb} {
+    transform: translateX(${TRACK_WIDTH - 2 * PADDING - THUMB_SIZE}px);
+  }
+  ${StyeldInput} + ${StyledThumb} {
+    transform: translateX(0px);
+  }
+`;
+
+export const StyledToggle = styled.div`
+  ${StyeldInput}:checked + ${StyledTrack} {
+    background-color: ${({ theme }) => theme.color.buttonPoint};
+  }
+  ${StyeldInput}:disabled + ${StyledTrack} {
+    background-color: ${({ theme }) => theme.color.buttonBG};
+  }
+  ${StyeldInput} + ${StyledTrack} {
+    background-color: ${({ theme }) => theme.color.buttonBG};
+  }
+
+  ${StyeldInput}:checked + ${StyledTrack} > ${StyledThumb} {
+    transform: translateX(${TRACK_WIDTH - 2 * PADDING - THUMB_SIZE}px);
+  }
+  ${StyeldInput} + ${StyledTrack} > ${StyledThumb} {
+    transform: translateX(0px);
+  }
+`;
