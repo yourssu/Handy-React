543211325e15897397fb801de6002e9ae6353b3a
nijuy <youjin6325@naver.com>
2023-10-31 19:18:10 +0900

    docs(component): useToast hook을 사용하는 story 추가

--- src/components/Toast/Toast.stories.tsx
+++ src/components/Toast/Toast.stories.tsx
@@ -1,6 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react';
 
+import { useToast } from '@/hooks/useToast';
+
 import { Toast } from './Toast';
+import { ToastDuration } from './Toast.type';
 
 const meta: Meta<typeof Toast> = {
   title: 'Component/Toast',
@@ -38,6 +41,29 @@ const ToastStory = ({ ...toastProps }) => {
   );
 };
 
+const HookTest = () => {
+  const toastProps = {
+    children: 'useToast를 사용한 토스트 메시지',
+    duration: 'long' as ToastDuration,
+  };
+  const { showToast, isShowToast } = useToast();
+
+  return (
+    <div
+      style={{ backgroundColor: '#c4c4c4', width: '390px', height: '644px', position: 'relative' }}
+    >
+      <button
+        onClick={() => {
+          showToast(toastProps.duration);
+        }}
+      >
+        버튼을 누르면 토스트가 발생합니다
+      </button>
+      {isShowToast && <Toast {...toastProps} />}
+    </div>
+  );
+};
+
 type Story = StoryObj<typeof Toast>;
 export const SingleLine: Story = {
   args: {
@@ -50,4 +76,7 @@ export const MultiLine: Story = {
     children: '줄 수가 두 줄 이상이 되는 토스트 메시지입니다. 좌측 정렬을 해주세요.',
   },
   render: ToastStory,
+};
+export const ToastHook: Story = {
+  render: HookTest,
 };
