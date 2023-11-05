b762fcb21dcc96bc88ecdf72b96c481bac15f552
Hanna922 <hanna.urssu@gmail.com>
2023-11-05 22:35:02 +0900

    feat: add ListItem.style.ts

---
+++ src/components/ListItem/ListItem.style.ts
@@ -0,0 +1,36 @@
+import { styled } from 'styled-components';
+
+import { typos } from '@/style';
+
+import { ListItemProps } from './ListItem.type';
+
+interface StyledListItemProps {
+  $isPressed: ListItemProps['isPressed'];
+  $width: ListItemProps['width'];
+}
+
+export const StyledListItem = styled.li<StyledListItemProps>`
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+  gap: 8px;
+  width: ${({ $width }) => $width};
+  height: 100%;
+  min-height: 48px;
+  font-size: 15px;
+  .icon {
+    width: 24px;
+    height: 24px;
+  }
+  .right-icon {
+    margin-left: auto;
+  }
+  ${typos.body1}
+  color: ${({ theme }) => theme.color.textSecondary};
+  background-color: ${({ $isPressed, theme }) =>
+    $isPressed ? theme.color.bgPressed : 'transparent'};
+  cursor: pointer;
+  &:hover {
+    background-color: ${({ theme }) => theme.color.bgPressed};
+  }
+`;
