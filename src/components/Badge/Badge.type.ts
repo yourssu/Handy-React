36641d0d8d9f89ff6a6ce7a01a20ba24c4c506e9
nijuy <youjin6325@naver.com>
2023-11-16 21:48:13 +0900

    fix: BadgeProps 타입이 HTMLAttribute 확장하도록 수정
    - 그리고 StyledBadge에 나머지 props를 넣어줬어요

--- src/components/Badge/Badge.type.ts
+++ src/components/Badge/Badge.type.ts
@@ -1,6 +1,6 @@
 import { SemanticBGColor } from '@/style';
 
-export interface BadgeProps {
+export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {
   color?: SemanticBGColor;
   children?: React.ReactNode;
   leftIcon?: React.ReactNode;
