3333b30c2dc23e682cf63b8f882da74c37a62302
혀느현스 <hyuns@hyuns.dev>
2023-11-21 00:38:51 +0900

    style: import { styled } 로 교체

--- src/components/Picker/Picker.style.ts
+++ src/components/Picker/Picker.style.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import { styled } from 'styled-components';
 
 export const StyledPicker = styled.div`
   position: relative;
--- src/components/Picker/PickerColumn.style.tsx
+++ src/components/Picker/PickerColumn.style.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import { styled } from 'styled-components';
 
 export const StyledPickerColumnContainer = styled.div`
   width: fit-content;
