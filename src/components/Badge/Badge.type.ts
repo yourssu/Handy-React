3113cba45894c3bf57ffe23fbebe2f5219c6f63d
nijuy <youjin6325@naver.com>
2024-02-07 18:13:30 +0900

    chore: Badge.type JSDoc 추가
    - 문서 내 속성 설명을 위해 JSDoc을 추가함

--- src/components/Badge/Badge.type.ts
+++ src/components/Badge/Badge.type.ts
@@ -1,7 +1,10 @@
 import { SemanticBGColor } from '@/style';
 
 export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {
+  /** 배경 색상 */
   color?: SemanticBGColor;
+  /** Badge 안에 들어갈 텍스트 */
   children?: React.ReactNode;
+  /** 텍스트 왼쪽에 들어갈 아이콘 */
   leftIcon?: React.ReactNode;
 }
