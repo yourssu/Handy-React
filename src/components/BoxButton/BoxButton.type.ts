fc652f84a5773e49090f669fd08749838bb6d735
Hanna922 <hanna.urssu@gmail.com>
2024-02-28 23:21:26 +0900

    refactor: apply import order, modify disabled type

--- src/components/BoxButton/BoxButton.type.ts
+++ src/components/BoxButton/BoxButton.type.ts
@@ -9,8 +9,6 @@ export interface BoxButtonProps extends React.ButtonHTMLAttributes<HTMLButtonEle
   variant: BoxButtonVariant;
   /** BoxButton의 모서리를 결정하는 속성 */
   rounding: BoxButtonRounding;
-  /** BoxButton의 비활성화 여부를 결정하는 속성, 비활성화 상태에는 pressed 효과가 존재하지 않습니다. */
-  isDisabled?: HTMLButtonElement['disabled'];
   /** BoxButton의 경고 여부를 결정하는 속성 */
   isWarned?: boolean;
   /** BoxButton의 왼쪽에 들어갈 아이콘 */
