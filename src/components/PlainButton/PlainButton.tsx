bffafa4128bafba8de86de5113d4780a59a02571
Hanna922 <hanna.urssu@gmail.com>
2024-02-28 23:26:02 +0900

    fix: props in BoxButton, PlainButton

--- src/components/PlainButton/PlainButton.tsx
+++ src/components/PlainButton/PlainButton.tsx
@@ -7,14 +7,14 @@ import { StyledPlainButton } from './PlainButton.style';
 import { PlainButtonProps } from '.';
 
 export const PlainButton = forwardRef<HTMLButtonElement, PlainButtonProps>(
-  ({ leftIcon, children, rightIcon, ...props }, ref) => {
+  ({ size, isPointed, isWarned, leftIcon, children, rightIcon, ...props }, ref) => {
     return (
       <StyledPlainButton
         ref={ref}
         disabled={props.disabled}
-        $size={props.size}
-        $isPointed={props.isPointed}
-        $isWarned={props.isWarned}
+        $size={size}
+        $isPointed={isPointed}
+        $isWarned={isWarned}
         {...props}
       >
         <IconContext.Provider
@@ -24,7 +24,7 @@ export const PlainButton = forwardRef<HTMLButtonElement, PlainButtonProps>(
         >
           <div className="icon left-icon">{leftIcon}</div>
         </IconContext.Provider>
-        {props.size !== 'large' && <span className="plainButton-child">{children}</span>}
+        {size !== 'large' && <span className="plainButton-child">{children}</span>}
         {!leftIcon && (
           <IconContext.Provider
             value={{
