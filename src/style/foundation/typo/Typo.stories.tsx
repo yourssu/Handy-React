5dc20056346d36abb587e5501132178f94cc8e8a
Hanna922 <hanna.urssu@gmail.com>
2024-03-24 02:19:43 +0900

    refactor: modify Typo docs

--- src/style/foundation/typo/Typo.stories.tsx
+++ src/style/foundation/typo/Typo.stories.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import { Primary as PrimaryBlock, Controls, Markdown } from '@storybook/blocks';
 import { Meta, StoryObj } from '@storybook/react';
 import { styled } from 'styled-components';
@@ -34,16 +36,34 @@ const TypoExample = styled.div<{ $typo: Typo }>`
 
 const TypoName = styled.div`
   ${(props) => props.theme.typo.caption0};
-  color: ${(props) => props.theme.color.textTertiary};
+  color: ${(props) => props.theme.color.textPrimary};
   width: 120px;
 `;
+
+const extractTypoDetails = (typoStyles: string) => {
+  const lines = typoStyles.split('\n');
+  const filteredLines = lines.filter(
+    (line: string) =>
+      line.includes('font-size:') || line.includes('font-weight:') || line.includes('line-height:')
+  );
+  return filteredLines.join('\n').trim();
+};
 
 const TypoStory = () => {
   return (
     <>
       {Object.keys(typos).map((key) => (
         <TypoRow key={key}>
-          <TypoName>{key}</TypoName>
+          <TypoName>
+            {extractTypoDetails(typos[key as Typo])
+              .split('\n')
+              .map((line) => (
+                <React.Fragment key={key}>
+                  {line}
+                  <br />
+                </React.Fragment>
+              ))}
+          </TypoName>
           <TypoExample key={key} $typo={key as Typo}>
             {key}
           </TypoExample>
