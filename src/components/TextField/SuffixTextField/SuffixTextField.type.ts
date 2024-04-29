c9c3561572853016859fc91fdcfe09b76c197827
Hanna922 <hanna.urssu@gmail.com>
2024-04-29 21:08:16 +0900

    @Hanna922 Merge branch 'develop' into feat/#70-TextField-props

--- src/components/TextField/SuffixTextField/SuffixTextField.type.ts
+++ src/components/TextField/SuffixTextField/SuffixTextField.type.ts
@@ -1,3 +1,6 @@
 import { TextFieldProps } from '../TextField.type';
 
-export interface SuffixTextFieldProps extends TextFieldProps {}
+export interface SuffixTextFieldProps extends Omit<TextFieldProps, 'searchPrefix'> {
+  /** TextField 오른쪽에 들어갈 텍스트 */
+  suffix?: string;
+}
