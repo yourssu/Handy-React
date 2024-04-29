25cff7c1be4de027c9bceb3f4a8311e842319347
nijuy <youjin6325@naver.com>
2024-04-29 20:01:06 +0900

    feat: SearchTextField 컴포넌트 추가

---
+++ src/components/TextField/SearchTextField/SearchTextField.tsx
@@ -0,0 +1,39 @@
+import { useTheme } from 'styled-components';
+
+import { IcSearchLine, IcXLine, IconContext } from '@/style';
+
+import { TextField } from '../TextField';
+
+import { SearchTextFieldProps } from './SearchTextField.type';
+
+export const SearchTextField = ({ onClickClearButton, ...props }: SearchTextFieldProps) => {
+  const theme = useTheme();
+
+  return (
+    <TextField
+      suffix={
+        <IconContext.Provider
+          value={{
+            color: theme.color.buttonNormal,
+            size: '1rem',
+          }}
+        >
+          <div className="suffix-icon clear-icon" onClick={onClickClearButton}>
+            <IcXLine />
+          </div>
+        </IconContext.Provider>
+      }
+      searchPrefix={
+        <IconContext.Provider
+          value={{
+            color: theme.color.textTertiary,
+            size: '1.5rem',
+          }}
+        >
+          <IcSearchLine />
+        </IconContext.Provider>
+      }
+      {...props}
+    />
+  );
+};
