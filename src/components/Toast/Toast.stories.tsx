26212b6c4d300de181b875a2538c908ce806fbad
nijuy <youjin6325@naver.com>
2024-05-05 19:15:53 +0900

    docs: Toast.stories.tsx 수정

--- src/components/Toast/Toast.stories.tsx
+++ src/components/Toast/Toast.stories.tsx
@@ -21,10 +21,6 @@ const meta: Meta<typeof Toast> = {
           <h2> 주의사항 </h2>
           <ol>
             <li>Toast의 width는 Toast 를 감싸는 컴포넌트의 width에 영향을 받습니다.</li>
-            <li>
-              Toast의 위치는 position: relative 속성이 설정된 가장 가까운 부모 컴포넌트에 의해
-              결정됩니다.
-            </li>
           </ol>
           <br />
           <Title>useToast</Title>
@@ -41,27 +37,13 @@ export default meta;
 
 const ToastStory = ({ ...toastProps }) => {
   return (
-    <div style={{ display: 'flex', gap: '10px', width: '780px', height: '300px' }}>
-      <div
-        style={{
-          backgroundColor: '#c4c4c4',
-          width: '50%',
-          position: 'relative',
-        }}
-      >
-        short duration toast (1.5s)
-        <Toast {...toastProps} />
-      </div>
-      <div
-        style={{
-          backgroundColor: '#c4c4c4',
-          width: '50%',
-          position: 'relative',
-        }}
-      >
-        long duration toast (3s)
-        <Toast duration="long" {...toastProps} />
-      </div>
+    <div
+      style={{
+        width: '500px',
+        height: '150px',
+      }}
+    >
+      <Toast {...toastProps} />
     </div>
   );
 };
@@ -76,15 +58,17 @@ const HookTest = () => {
   return (
     <div
       style={{
-        backgroundColor: '#c4c4c4',
-        width: '1920px',
-        height: '1080px',
+        width: '500px',
+        height: '300px',
+        display: 'flex',
+        justifyContent: 'center',
       }}
     >
       <button
         onClick={() => {
           showToast(toastProps.duration);
         }}
+        style={{ height: 'fit-content' }}
       >
         버튼을 누르면 토스트가 발생합니다
       </button>
@@ -97,12 +81,14 @@ type Story = StoryObj<typeof Toast>;
 export const SingleLine: Story = {
   args: {
     children: '토스트 메시지',
+    duration: 'short',
   },
   render: ToastStory,
 };
 export const MultiLine: Story = {
   args: {
     children: '줄 수가 두 줄 이상이 되는 토스트 메시지입니다.\n좌측 정렬을 해주세요.',
+    duration: 'short',
   },
   render: ToastStory,
 };
