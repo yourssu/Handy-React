4d31c48a4bfdd83abe9cfa023101496518644a7c
nijuy <youjin6325@naver.com>
2024-05-09 23:22:55 +0900

    rename: OnlyBGColor -> OnlyItemBGColor

--- src/components/Badge/Badge.type.ts
+++ src/components/Badge/Badge.type.ts
@@ -1,8 +1,8 @@
-import { SemanticBGColor } from '@/style';
+import { SemanticItemBGColor } from '@/style';
 
 export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement> {
   /** 배경 색상 */
-  color?: SemanticBGColor;
+  color?: SemanticItemBGColor;
   /** Badge 안에 들어갈 텍스트 */
   children?: React.ReactNode;
   /** 텍스트 왼쪽에 들어갈 아이콘 */
