0dcc24e0db8baa392eec52a924c9bc21e28d0215
Hanna922 <hanna.urssu@gmail.com>
2024-05-16 16:44:46 +0900

    fix: load spoqa font with webfontloader

--- src/style/globalStyle/GlobalStyle.tsx
+++ src/style/globalStyle/GlobalStyle.tsx
@@ -1,10 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  custom: {
+    families: ['Spoqa Han Sans Neo'],
+    urls: ['//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSansNeo.css'],
+  },
+});
 
 export const GlobalStyles = createGlobalStyle`
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-        font-family: 'Spoqa Han Sans Neo', 'sans-serif';
-    }
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+  }
 `;
