4a0973787831d9846e94b67f55232a183b14200d
nijuy <youjin6325@naver.com>
2024-05-16 23:30:42 +0900

    refactor: `StyledSearchPrefixContainer` 삭제

--- src/components/TextField/TextField.style.ts
+++ src/components/TextField/TextField.style.ts
@@ -16,10 +16,6 @@ export const StyledSuffixIconContainer = styled.div`
   display: none;
 `;
 
-export const StyledSearchPrefixContainer = styled.div`
-  display: flex;
-`;
-
 export const StyledTextFieldWrapper = styled.div<StyledTextFieldProps>`
   width: ${({ $width }) => $width};
   height: 46px;
