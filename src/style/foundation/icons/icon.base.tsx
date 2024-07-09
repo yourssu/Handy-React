2c32704a30e11a3949c5ce6e57811d96901eb1ae
nijuy <youjin6325@naver.com>
2024-07-09 23:56:15 +0900

    remove: IconContext

--- src/style/foundation/icons/icon.base.tsx
+++ src/style/foundation/icons/icon.base.tsx
@@ -1,27 +1,18 @@
-import { forwardRef, useContext } from 'react';
+import { forwardRef } from 'react';
 
-import { IconContext } from './icon.context';
 import { IconProps } from './icon.type';
 
 export const IconBase = forwardRef<SVGSVGElement, IconProps>((props, ref) => {
   const { size, color, mirrored, children, ...rest } = props;
 
-  const {
-    color: contextColor = 'current',
-    size: contextSize,
-    mirrored: contextMirrored = false,
-    ...restContext
-  } = useContext(IconContext);
-
   return (
     <svg
       ref={ref}
       xmlns="http://www.w3.org/2000/svg"
-      width={size ?? contextSize}
-      height={size ?? contextSize}
-      fill={color ?? contextColor}
-      transform={mirrored || contextMirrored ? 'scale(-1, 1)' : undefined}
-      {...restContext}
+      width={size}
+      height={size}
+      fill={color}
+      transform={mirrored ? 'scale(-1, 1)' : undefined}
       {...rest}
     >
       {children}
