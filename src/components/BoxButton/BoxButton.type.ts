97be011c0e4a8c53ddea7d24d5137793df123d93
nijuy <youjin6325@naver.com>
2024-07-18 00:31:19 +0900

    feat: create BoxButton.type

---
+++ src/components/BoxButton/BoxButton.type.ts
@@ -0,0 +1,17 @@
+export type BoxButtonSize = 'Xlarge' | 'Large' | 'Medium' | 'Small' | 'Xsmall' | 'Xxsmall';
+export type BoxButtonHierarchy = 'primary' | 'secondary' | 'tertiary';
+
+export interface BoxButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  /** BoxButton의 크기를 결정하는 속성 */
+  size: BoxButtonSize;
+  /** BoxButton의 모양을 결정하는 속성 */
+  hierarchy: BoxButtonHierarchy;
+  /** BoxButton의 왼쪽에 들어갈 아이콘 */
+  leftIcon?: React.ReactNode;
+  /** BoxButton의 내용 */
+  children?: React.ReactNode;
+  /** BoxButton의 오른쪽에 들어갈 아이콘 */
+  rightIcon?: React.ReactNode;
+  /** BoxButton의 width */
+  width?: string | number;
+}
