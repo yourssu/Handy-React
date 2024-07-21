98a6a2f7d5b089c61e16b49104042f1644dc2c39
nijuy <youjin6325@naver.com>
2024-07-22 00:58:51 +0900

    Revert "feat: create semantic color for Button"
    This reverts commit 76512765578bf37874a7e675f6603e18ffc6fa6f.

--- src/style/foundation/color/semanticColor/semanticColor.type.ts
+++ src/style/foundation/color/semanticColor/semanticColor.type.ts
@@ -1,15 +1,103 @@
-export type SemanticButtonColor<ButtonName extends string> =
-  | `button${ButtonName}Enabled`
-  | `button${ButtonName}Pressed`
-  | `button${ButtonName}Disabled`;
+// https://www.figma.com/file/yVXvVvBKZnG9BBsvdC3J5B/%5B0.-Common%5D-Yourssu-Design-System?type=design&node-id=1%3A461&mode=design&t=KtrwDpGrj62wFgzG-1
 
-export type SemanticBoxButtonColor =
-  | SemanticButtonColor<'BoxPrimary'>
-  | SemanticButtonColor<'BoxSecondary'>
-  | SemanticButtonColor<'BoxTertiary'>;
+export type SemanticBackgroundColor =
+  | 'bgNormal'
+  | 'bgElevated'
+  | 'bgRecomment'
+  | 'bgPressed'
+  | 'bgSelected'
+  | 'bgNormalDark'
+  | 'bgElevatedDark'
+  | 'bgDimDark';
 
-export type SemanticTextButtonColor =
-  | SemanticButtonColor<'TextPrimary'>
-  | SemanticButtonColor<'TextSecondary'>;
+export type SemanticTextColor =
+  | 'textPrimary'
+  | 'textSecondary'
+  | 'textTertiary'
+  | 'textDisabled'
+  | 'textBright'
+  | 'textPointed'
+  | 'textWarned';
 
-export type SemanticColor = SemanticBoxButtonColor | SemanticTextButtonColor;
+export type SemanticDimColor = 'dimNormal' | 'dimThick' | 'dimThickBright';
+
+export type SemanticBorderColor = 'borderThin' | 'borderNormal' | 'borderThick';
+
+export type SemanticButtonColor =
+  | 'buttonNormal'
+  | 'buttonNormalPressed'
+  | 'buttonBG'
+  | 'buttonEmojiBG'
+  | 'buttonBright'
+  | 'buttonDisabled'
+  | 'buttonDisabledBG'
+  | 'buttonPoint'
+  | 'buttonPointPressed'
+  | 'buttonPointBG'
+  | 'buttonWarned'
+  | 'buttonWarnedPressed'
+  | 'buttonWarnedBG';
+
+export type SemanticBottomBarColor = 'bottomBarNormal' | 'bottomBarSelected';
+
+export type SemanticInputFieldColor = 'inputFieldNormal' | 'inputFieldElevated';
+
+export type SemanticToastColor = 'toastBG';
+
+export type SemanticPressedColor = 'pressed';
+
+export type SemanticToolTipColor = 'tooltipBG' | 'tooltipPoint';
+
+export type SemanticPickerColor = 'dimPickerReverse';
+
+export type SemanticShadowColor = 'shadowThin' | 'shadowNormal';
+
+export type SemanticLogoColor = 'pointIndigo' | 'pointViolet' | 'pointYellow';
+
+type SemanticItemColorVariants<ColorName extends string> =
+  | `${ColorName}Primary`
+  | `${ColorName}BG`
+  | `${ColorName}Text`;
+
+export type SemanticMonoItemColor = SemanticItemColorVariants<'monoItem'>;
+export type SemanticLimeItemColor = SemanticItemColorVariants<'limeItem'>;
+export type SemanticGreenItemColor = SemanticItemColorVariants<'greenItem'>;
+export type SemanticEmeraldItemColor = SemanticItemColorVariants<'emeraldItem'>;
+export type SemanticAquaItemColor = SemanticItemColorVariants<'aquaItem'>;
+export type SemanticBlueItemColor = SemanticItemColorVariants<'blueItem'>;
+export type SemanticIndigoItemColor = SemanticItemColorVariants<'indigoItem'>;
+export type SemanticVioletItemColor = SemanticItemColorVariants<'violetItem'>;
+export type SemanticPurpleItemColor = SemanticItemColorVariants<'purpleItem'>;
+export type SemanticPinkItemColor = SemanticItemColorVariants<'pinkItem'>;
+
+export type SemanticItemColor =
+  | SemanticMonoItemColor
+  | SemanticLimeItemColor
+  | SemanticGreenItemColor
+  | SemanticEmeraldItemColor
+  | SemanticAquaItemColor
+  | SemanticBlueItemColor
+  | SemanticIndigoItemColor
+  | SemanticVioletItemColor
+  | SemanticPurpleItemColor
+  | SemanticPinkItemColor;
+
+export type SemanticColor =
+  | SemanticBackgroundColor
+  | SemanticTextColor
+  | SemanticDimColor
+  | SemanticBorderColor
+  | SemanticButtonColor
+  | SemanticBottomBarColor
+  | SemanticInputFieldColor
+  | SemanticToastColor
+  | SemanticPressedColor
+  | SemanticToolTipColor
+  | SemanticPickerColor
+  | SemanticShadowColor
+  | SemanticLogoColor
+  | SemanticItemColor;
+
+// Utility Types
+type OnlyItemBGColor<T> = T extends `${string}ItemBG` ? T : never;
+export type SemanticItemBGColor = OnlyItemBGColor<SemanticColor>;
