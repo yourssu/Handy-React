41bc3bb58c863b7315914a981396127e716b5e07
Sanghyeok Park <fecapark@gmail.com>
2024-07-23 17:00:14 +0900

    feat: Checkbox 모듈 export

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export { YDSWrapper } from './YDSWrapper';
 export type { YDSWrapperProps } from './YDSWrapper';
+
+export { Checkbox } from './Checkbox';
+export type { CheckboxProps, CheckboxSizeType } from './Checkbox';
