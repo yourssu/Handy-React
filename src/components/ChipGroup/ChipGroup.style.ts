c30003e4a298750e0650415ac6d473989aa4e440
Sanghyeok Park <fecapark@gmail.com>
2024-07-29 15:52:39 +0900

    refactor: chip group의 gap을 token으로 수정

--- src/components/ChipGroup/ChipGroup.style.ts
+++ src/components/ChipGroup/ChipGroup.style.ts
@@ -3,6 +3,6 @@ import { styled } from 'styled-components';
 export const StyledChipGroupContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
-  row-gap: 10px; // TODO: semantic theme 적용 필요
-  column-gap: 8px; // TODO: semantic theme 적용 필요
+  row-gap: ${({ theme }) => theme.primitive.number[10]}px;
+  column-gap: ${({ theme }) => theme.primitive.number[8]}px;
 `;
