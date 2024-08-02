3deded274446fc5753b1d0bfc3d1c52b9824875e
2wndrhs <2wndrhs@gmail.com>
2024-08-02 18:22:00 +0900

    chore: index 파일 업데이트

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,3 +10,5 @@ export type { TextButtonProps } from './TextButton';
 export { Checkbox } from './Checkbox';
 export type { CheckboxProps, CheckboxSizeType } from './Checkbox';
 
+export type * from './Fab';
+export { Fab } from './Fab';
