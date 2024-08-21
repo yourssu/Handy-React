b4ade1430919e355b41917c54957ab80019f1c57
seocylucky <seocylucky@github.com>
2024-08-21 22:55:05 +0900

    refactor: Textarea 타입 중복 정의 제거
    (plaholder, disabled, maxLength)

--- src/components/Textarea/Textarea.type.ts
+++ src/components/Textarea/Textarea.type.ts
@@ -1,10 +1,7 @@
 export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
   width: string;
   height: string;
-  maxLength?: number;
   helperText?: string;
-  placeholder?: string;
-  disabled?: boolean;
   error?: boolean;
   value?: string;
   onValueChange?: (value: string) => void;
