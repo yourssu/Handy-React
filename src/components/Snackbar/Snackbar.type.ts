40f2c8f29378d52340c43cbad72fbfeff284c622
seocylucky <seocylucky@github.com>
2024-10-31 01:44:09 +0900

    fix: SnackbarWithoutClosingProps 타입 생성

--- src/components/Snackbar/Snackbar.type.ts
+++ src/components/Snackbar/Snackbar.type.ts
@@ -3,14 +3,15 @@ export type SnackbarHeightType = 1 | 2;
 export type SnackbarPosition = 'left' | 'center' | 'right' | 'full-width';
 
 export interface SnackbarProps {
-  id: string;
   type?: SnackbarType;
   width?: string;
   margin?: string;
   message: string;
-  onClose?: (id: string) => void;
+  onClose?: () => void;
   duration?: number;
   position?: SnackbarPosition;
   isClosing: boolean;
   $heightType?: SnackbarHeightType;
 }
+
+export type SnackbarWithoutClosingProps = Omit<SnackbarProps, 'isClosing'>;
